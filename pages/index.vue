7f0250d0d6c8b6c6b857bea5c8454844bebbcf0e
OlaRabc <olar8871@gmail.com>
2024-08-30 12:57:30 +0200

    Google Analytics test 2

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
         <div
           class="mx-auto mt-12 w-fit bg-white border rounded-xl px-8 py-16 shadow max-w-4xl"
         >
-          <h4 class="font-bold">Witaj w Hotelu Fun&Relax nad Morzem Bałtyckim w Świnoujściu!</h4>
+          <h4 class="font-bold">Witaj w Hotelu Fun&Relax nad Morzem Bałtyckim w Świnoujściu!!</h4>
           <p class="mb-2">
             Zanurz się w wyjątkowym świecie luksusu i relaksu, gdzie wypoczynek
             staje się prawdziwą przyjemnością. Nasz hotel to idealne miejsce na
