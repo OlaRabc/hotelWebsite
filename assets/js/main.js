8761c3d8309750193099522e03dbb5f24be7aee1
OlaRabc <olar8871@gmail.com>
2024-09-01 13:51:43 +0200

    google analitics uid test

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,13 @@ export function gtagEvent(
   eventLabel = ""
 ) {
     if (typeof window !== "undefined" && window.gtag) {
+      const eventTime = new Date().toISOString();
+
       window.gtag(interactionType, eventType, {
         event_category: eventCategory,
         event_label: eventLabel,
+        user_id: 'uid_test',
+        event_label: `submitted_at_${eventTime}`,
         value: 1,
       });
     } else {
