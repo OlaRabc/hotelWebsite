77ea148b6a97f8adbd6d27339107b3befc1cb733
OlaRabc <olar8871@gmail.com>
2024-10-22 08:57:30 +0200

    Added Error Pages

---
+++ components/molecue/FrError.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+      role="alert"
+      aria-live="assertive"
+      class="flex flex-col md:flex-row md:justify-around items-center min-h-screen max-md:text-center"
+    >
+      <h1
+        type="h1"
+        variant="h1"
+        class="!text-[10rem] mt-32 md:-mt-64 max-md:mb-32 font-bold"
+      >
+        {{ props.error }}
+      </h1>
+  
+      <div>
+        <h2 class="mb-2 font-bold text-5xl">UPS!</h2>
+  
+        <slot />
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps({
+    error: {
+      type: String,
+      required: true,
+    },
+  });
+  </script>
---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+    <FrError404 />
+
+    <FrError404 v-if="(error.statusCode = '404')" />
+    <FrError500 v-else-if="(error.statusCode = '500')" />
+    <FrErrorUndefined v-else :error="error.statusCode"/>
+  </template>
+  
+  <script>
+  export default { props: ["error"] };
+  </script>
