b594b9bde8e3acfe5e4850745dbf52d789a90d21
OlaRabc <olar8871@gmail.com>
2024-10-24 19:44:47 +0200

    Added mobile nav

--- components/molecue/FrNav.vue
+++ components/molecue/FrNav.vue
@@ -1,63 +1,94 @@
 <template>
-  <div>
-    <nav
-      class="bg-white w-full pt-4 flex flex-col items-center sm:flex-row sm:justify-between bg-[#C4B976]"
-    >
-      <div class="link w-20 h-full ml-4 mb-4">
-        <NuxtLink @click="navigateTo(`/`)" class="cursor-pointer">
-          <img src="/public/images/logo.png" class="w-full h-full" />
-        </NuxtLink>
-      </div>
+  <nav class="relative bg-white w-full pt-4 flex justify-between">
+    <div class="link w-20 h-full ml-4 mb-4">
+      <NuxtLink to="/" @click="navigateTo" class="cursor-pointer">
+        <img src="/public/images/logo.png" class="w-full h-full" />
+      </NuxtLink>
+    </div>
 
-      <ul
-        class="flex justify-around sm:justify-end flex-wrap w-full text-lg sm:text-xl items-end mb-2"
-      >
-        <li
+    <div class="hidden md:block">
+      <ul class="flex justify-between text-lg font-bold mt-2">
+        <NuxtLink
           v-for="item in pageList"
-          class="py-2 hover:text-[#C4B976] self-center"
+          :key="item.name"
+          :to="item.src"
+          class="relative px-4 my-4 text-center text-nowrap cursor-pointer hover:text-gold-600 before:content-[' '] before:absolute before:bottom-0 before:left-1/2 before:-translate-x-1/2 before:w-[80%] before:h-[2px] before:rounded-full before:bg-gold-600 before:opacity-0 transition-colors delay-75 duration-150 hover:before:opacity-100 before:transition-opacity before:delay-75 before:duration-150"
+          active-class="text-gold-600"
+          @click="navigateTo"
         >
-          <NuxtLink @click="navigateTo(item.scr)" class="px-4 sm:px-8 text-center text-nowrap cursor-pointer">
+          <li>
             {{ item.name }}
-          </NuxtLink>
-        </li>
+          </li>
+        </NuxtLink>
       </ul>
-    </nav>
-  </div>
+    </div>
+
+    <button class="block md:hidden text-4xl mr-8 -mt-2" @click="openNav">
+      <ClientOnly><font-awesome-icon icon="bars" /></ClientOnly>
+    </button>
+    <div
+      class="absolute md:hidden top-0 flex flex-col h-screen w-full z-[100] bg-white transition-all delay-75 duration-300"
+      :class="isNavOpen ? ' right-0 md:-right-full' : '-right-full'"
+    >
+      <button class="text-4xl self-end mr-8 mt-8 mb-6 p-1" @click="closeNav">
+        <ClientOnly>
+          <font-awesome-icon icon="xmark" />
+        </ClientOnly>
+      </button>
+
+      <ul class="w-full text-lg sm:text-2xl items-end mb-4 font-bold">
+        <NuxtLink
+          v-for="item in pageList"
+          :to="item.src"
+          class="px-4 sm:px-8 py-4 w-full text-center text-nowrap cursor-pointer hover:text-gold-600"
+          active-class="text-gold-600"
+          @click="navigateTo"
+        >
+          <li>
+            {{ item.name }}
+          </li>
+        </NuxtLink>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts" setup>
 import { gtagEvent } from "@/assets/js/main";
-const router = useRouter();
 
-const navigateTo = (to: string) => {
-  router.push(to);
+const navigateTo = () => {
+  closeNav();
   // gtagEvent("event", "page_view", "navigation", "Hotel description");
 };
 
 const pageList = [
   {
     name: "Cennik",
-    scr: "cennik",
+    src: "cennik",
   },
   {
     name: "Restauracja",
-    scr: "restauracja",
+    src: "restauracja",
   },
   {
     name: "Strefa Relaksu",
-    scr: "spa",
+    src: "spa",
   },
   {
     name: "Atrakcje",
-    scr: "atrakcje",
+    src: "atrakcje",
   },
   {
     name: "Kontakt",
-    scr: "kontakt",
+    src: "kontakt",
   },
   {
     name: "Hotel",
-    scr: "hotel",
-  }
+    src: "hotel",
+  },
 ];
+
+const isNavOpen: boolean = ref(false);
+const openNav = () => (isNavOpen.value = true);
+const closeNav = () => (isNavOpen.value = false);
 </script>
