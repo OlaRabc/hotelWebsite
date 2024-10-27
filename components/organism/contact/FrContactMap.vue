f6c5304402648f91db4e6e684f068562d4bc31bc
OlaRabc <olar8871@gmail.com>
2024-10-27 14:12:14 +0100

    Fixed Contact And Restaurant Page

---
+++ components/organism/contact/FrContactMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="border bg-white lg:rounded-xl lg:w-1/2 max-lg:-mx-16 mb-8 lg:mb-0 h-[400px] lg:h-auto shadow-md"
+    @click="gtagEvent('event', 'contact', 'button', 'location_map')"
+  >
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7905.0064157277!2d14.37818064011855!3d53.91091109032235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47aa58cdcdae3301%3A0x1fe03a970a6e4dc8!2zxZp3aW5vdWrFm2NpZQ!5e0!3m2!1spl!2spl!4v1721043307924!5m2!1spl!2spl"
+      class="w-full h-full lg:rounded-xl"
+      style="border: 0"
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { gtagEvent } from "@/assets/js/main";
+</script>
---
+++ components/organism/restaurant/FrRestaurantMenuOfDay.vue
@@ -0,0 +1,32 @@
+<template>
+  <FrBox class="w-1/2 mb-12">
+    <h3 class="font-bold text-2xl mb-8">Menu dnia (w każdą niedzielę)</h3>
+    <div v-for="item in menuDay" class="flex justify-between m-2">
+      <div class="my-4">
+        {{ item.title }}
+        <div class="text-sm">{{ item.subtitle }}</div>
+      </div>
+      <div class="my-auto">{{ item.prices }} zł</div>
+    </div>
+  </FrBox>
+</template>
+
+<script setup lang="ts">
+const menuDay = [
+  {
+    title: "Rosół z kaczki",
+    subtitle: "",
+    prices: 15,
+  },
+  {
+    title: "Sznycel z indyka ",
+    subtitle: "z frytkami/ziemniakami i surówką z marchewki",
+    prices: 30,
+  },
+  {
+    title: "Szarlotka na ciepło",
+    subtitle: "z sosem czekoladowym, lodami i bitą śmietaną",
+    prices: 60,
+  },
+];
+</script>
